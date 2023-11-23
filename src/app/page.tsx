a26471fd2fa1e98ba1bcf94201a1db562216bc10
Jenssen <jenssen.lee@gmail.com>
2023-11-23 17:46:32 +0800

    Add func to update search term and search registry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,44 @@
+'use client';
 import { Box, Button, TextField, Typography } from '@mui/material'
+import axios from 'axios';
 
-export default function Home() {
+const state = {
+  searchTerm: ''
+}
+
+function searchTermHandler(event: any) {
+  state.searchTerm = event?.target.value
+}
+
+async function searchNpmRegistry(event: any) {
+  event.preventDefault(); 
+  console.log('searchNpmRegistry')
+
+  const res = await axios.get(`https://registry.npmjs.org/${state.searchTerm}`)
+  console.log('res', res)
+}
+
+export default function searchNpmRegistryComponent() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Box sx={{ width: '100%', maxWidth: 300 }}>
+      <Box
+        component="form"
+        sx={{ width: '100%', maxWidth: 300 }}
+        noValidate
+        autoComplete="off"
+        onSubmit={searchNpmRegistry}
+      >
         <Typography variant="h6" gutterBottom>NPM registry</Typography>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <TextField id="outlined-basic" label="Search" variant="outlined" fullWidth />
-          <Button variant="contained" size="medium" style={{ marginLeft: '8px', backgroundColor: '#1565C0' }}>Submit</Button>
+          <TextField id="outlined-basic" label="Search" variant="outlined" fullWidth onChange={searchTermHandler} />
+          <Button 
+            variant="contained" 
+            size="medium" 
+            style={{ marginLeft: '8px', backgroundColor: '#1565C0' }} 
+            type='submit'
+          >
+            Submit
+          </Button>
         </div>
       </Box>
     </main>
