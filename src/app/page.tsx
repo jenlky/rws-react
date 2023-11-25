fe699f0366aa7781fd0f97849d085f5d74165c65
Jenssen <jenssen@wenconsulting.com>
2023-11-25 18:14:08 +0800

    Add interfaces, retrieve name, date and author name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,61 @@
 'use client';
 import { Box, Button, TextField, Typography } from '@mui/material'
 import axios from 'axios';
+import { useState } from 'react';
 
-const state = {
-  searchTerm: ''
+interface npmObject {
+  flags?: object
+  package: Package
+  score?: object
+  searchScore?: string
 }
 
-function searchTermHandler(event: any) {
-  state.searchTerm = event?.target.value
+interface Author {
+  name: string
 }
 
-async function searchNpmRegistry(event: any) {
-  event.preventDefault(); 
-  console.log('searchNpmRegistry')
-
-  const res = await axios.get(`https://registry.npmjs.org/${state.searchTerm}`)
-  console.log('res', res)
+interface Package {
+  author: Author
+  date: string
+  description?: string
+  keywords?: string[]
+  links?: object
+  maintainers?: object[]
+  name: string
+  publisher?: object
+  scope?: string
+  version?: string
 }
 
 export default function searchNpmRegistryComponent() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [data, setData] = useState<npmObject[]>([])
+
+  function searchTermHandler(event: any) {
+    setSearchTerm(event?.target.value)
+  }
+  
+  async function searchNpmRegistry(event: any) {
+    event.preventDefault(); 
+    console.log('searchNpmRegistry')
+  
+    const res = await axios.get(`https://registry.npmjs.org/-/v1/search?text=${searchTerm}`)
+    console.log('res', res)
+    setData(res.data.objects)
+  }
+  
+  function SearchResults() {
+    return data.map(pkg => {
+      return (
+        <div>
+          {pkg.package.name}
+          {pkg.package.date}
+          {pkg.package.author?.name}
+        </div>
+      )
+    })
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Box
@@ -41,6 +78,7 @@ export default function searchNpmRegistryComponent() {
           </Button>
         </div>
       </Box>
+      <SearchResults />
     </main>
   )
 }
