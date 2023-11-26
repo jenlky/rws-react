fe14c67eb4df5e6c907145db74c726f187c0fa5d
Jenssen <jenssen.lee@gmail.com>
2023-11-26 21:06:15 +0800

    Add error snackbar, update display, get offset working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,100 @@
 'use client';
-import { Alert, Box, Button, CircularProgress, TextField, Typography } from '@mui/material'
+import { Alert, Box, Button, Pagination, Snackbar, TextField, Typography } from '@mui/material'
 import axios from 'axios';
 import { useState } from 'react';
 import './style.css'
 import { NpmObject } from './model/npm-registry';
 
+// global variable
+const resultsPerPage = 10
+
 export default function searchNpmRegistryComponent() {
   const [searchTerm, setSearchTerm] = useState('')
   const [data, setData] = useState<NpmObject[]>([])
+  const [total, setTotal] = useState(0)
+  const [toDisplayPage, setToDisplayPage] = useState(false)
+  const [openErrorMsg, setOpenErrorMsg] = useState(false)
+  const [page, setPage] = useState(1);
 
   function searchTermHandler(event: any) {
     setSearchTerm(event?.target.value)
   }
   
-  function formatDate(dateString: string){
+  function formatDate(dateString: string) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(dateString).toLocaleDateString([], options);
   }
 
   async function searchNpmRegistry(event: any) {
-    event.preventDefault();   
-    const res = await axios.get(`https://registry.npmjs.org/-/v1/search?text=${searchTerm}`)
+    event.preventDefault();
+    const res = await axios.get(`https://registry.npmjs.org/-/v1/search?text=${searchTerm}&size=${resultsPerPage}&from=0`)
     console.log('res', res)
     setData(res.data.objects)
+    setTotal(res.data.total)
+
+    if (res.data.total > 0) {
+      setToDisplayPage(true)
+    } else {
+      setOpenErrorMsg(true);
+      setToDisplayPage(false)
+    }
   }
-  
+
   function SearchResults() {
     return data.map(pkg => {
       return (
-        <div className='search-result-row'>
+        <div className='search-result-row' key={pkg.package.name}>
           <div className='search-result-row-top'>
-            <div>{pkg.package.name}</div>
-            <div>{formatDate(pkg.package.date)}</div>
+            <div>
+              <div>
+                <span className='bold'>Package: </span>
+                <span>{pkg.package.name}</span>
+              </div>
+              <div>
+                <span className='bold'>Author: </span>
+                <span className='italic'>{pkg.package.author?.name}</span>            
+              </div>
+            </div>
+            <div>
+              <div className='updated-date'>Updated date</div>
+              <div className='italic'>{formatDate(pkg.package.date)}</div>
+            </div>
           </div>
-          <div>{pkg.package.author?.name}</div>
         </div>
       )
     })
   }
 
+  async function handlePageChange (event: any, value: number) {
+    setPage(value)
+    const offset = resultsPerPage * (value - 1)
+    console.log('offset', offset)
+
+    const res = await axios.get(`https://registry.npmjs.org/-/v1/search?text=${searchTerm}&size=${resultsPerPage}&from=${offset}`)
+    console.log('res handlePageChange', res)
+  
+    setData(res.data.objects)
+    setTotal(res.data.total)
+  }
+
+  function Page() {
+    const numOfPages = Math.ceil(total/resultsPerPage)
+    if (toDisplayPage) {
+      return (
+        <Pagination className='pagination' count={numOfPages} onChange={handlePageChange} /> 
+      )
+    }
+  }
+
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenErrorMsg(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-20" style={{ marginBottom: '10px' }}>
       <Box
         component="form"
         sx={{ width: '100%', maxWidth: 500 }}
@@ -62,6 +116,12 @@ export default function searchNpmRegistryComponent() {
         </div>
       </Box>
       <SearchResults />
+      <Page />
+      <Snackbar open={openErrorMsg} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          No npm packages were found!
+        </Alert>
+      </Snackbar>
     </main>
   )
 }
